caeb51f12c9aa55ca2729b87e9d9d2cd16a88397
suguru1107 <r202103323ct@jindai.jp>
2023-01-27 03:28:16 +0900

    for

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  int i = 100;
-  printf("hello world");
-  printf("%d", i);
+  int i = 0;
+  for (i < 10; i++;) {
+    printf("hello world");
+    printf("%d", i);
+  }
   return 0;
 }
